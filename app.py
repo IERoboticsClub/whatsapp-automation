cb1bd99b272d185898079e6f941ba8c8192defc0
haxy <zaidsaheb4@gmail.com>
2024-02-09 11:53:04 +0100

    initial funcitonality

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ st.title('Whatsapp Automation')
 st.text('<3')
 
 form = st.form(key='my_form')
-event = form.selectbox('Select an event', ('Workshop', 'POW'))
+event = form.selectbox('Select an event', ('Workshop', 'POW'), index=None)
 datetime = form.date_input('What day the event?',value=None,format="DD/MM/YYYY")
-time = form.time_input('What time the event?',value=None)
+time = form.time_input('What time is the event?',value=None)
+st.write(f'There is a {event}, at {time}, on {datetime}')
 
 form.form_submit_button('Submit')
