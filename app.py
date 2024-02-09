a0ba93c806028198f09f5a4a9df418e50214ccd5
haxy <zaidsaheb4@gmail.com>
2024-02-09 11:47:39 +0100

    added fields initially

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 import streamlit as st
+import datetime
 
 st.title('Whatsapp Automation')
 st.text('<3')
+
+form = st.form(key='my_form')
+event = form.selectbox('Select an event', ('Workshop', 'POW'))
+datetime = form.date_input('What day the event?',value=None,format="DD/MM/YYYY")
+time = form.time_input('What time the event?',value=None)
+
+form.form_submit_button('Submit')
